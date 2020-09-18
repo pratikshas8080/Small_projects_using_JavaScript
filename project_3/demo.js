fbcfa19b605a29e2a9edc518469d87431d2e4cb1
pratiksha sawandkar <63863917+pratikshas8080@users.noreply.github.com>
2020-09-18 11:30:44 +0530

    Add files via upload

---
+++ project_3/demo.js
@@ -0,0 +1,42 @@
+import TaskController from "https://cdn.jsdelivr.net/gh/iagocalazans/dailytasker/assets/js/controllers/taskController.js";
+
+const form = document.getElementById( "newTaskForm" );
+const taskController = new TaskController( );
+const taskList = document.getElementById( "task-list" );
+const refreshEvent = new Event( "refresh" );
+
+// Function to control Form Events
+( function ( ) {
+    form.addEventListener( "submit", function ( e ) {
+        e.preventDefault( );
+        taskController.submit( e );
+        form.reset( );
+    } );
+
+    form.addEventListener( "reset", function ( e ) {
+        const collapseItem = document.getElementById( "collapseItem" );
+        collapseItem.classList.remove( "show" );
+    } );
+} )( );
+
+// Function to control List Refresh
+( function ( ) {
+    taskList.addEventListener( "refresh", ( ) => taskController.refresh( ) );
+
+    setInterval( refresh, 1 * 60000 );
+} )( );
+
+//Function Refresh, to dispatch from Interval
+function refresh( ) {
+    taskList.dispatchEvent( refreshEvent );
+}
+
+//Function to first refresh after Window Load
+( function ( ) {
+    window.addEventListener( "load", ( ) => taskController.refresh( ) );
+} )( );
+
+( function ( ) {
+    const clearBtn = document.getElementById( "clearAll" );
+    clearBtn.addEventListener( "click", ( ) => taskController.clear( ) );
+} )( );
